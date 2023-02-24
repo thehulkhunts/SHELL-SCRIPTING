161f72b1ee4ef7e8988d1cad8af6a0fe544730e3
root <root@ip-172-31-45-228.ap-south-1.compute.internal>
2023-02-24 07:26:36 +0000

    if-else condition

---
+++ if-else.sh
@@ -0,0 +1,14 @@
+ #! /bin/bash
+  
+ echo " simple if-else: "
+
+ echo " enter the number: "
+
+ read -r n
+ if [ $n -gt 0 ];
+ then
+	 echo " entered the number is positive number "
+ else
+	 echo " entered number is negative number "
+ fi
+
