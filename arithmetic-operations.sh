04766f553ffe934ab990fdc02b6b489323154e38
vinay <vinay@gmaial.com>
2023-02-28 08:09:02 +0000

    arithmetic-operations

---
+++ arithmetic-operations.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#Author: vinay jadhav
+#create Date:
+#purpose: Arithmetic Operaions
+# START #
+
+echo -e " read the valaue a : \c "
+read -r a
+
+echo -e " read the value b: \c "
+read -r b
+
+echo "the value of a+b is: $(($a+$b)) "
+echo " the value of a-b is: $(($a-$b)) "
+echo " the value of a/b is: $(($a/$b)) "
+echo " the value of a*b is : $(($a*$b)) "
+echo " the value of a%b is: $(($a%$b)) "
+
+echo "  arithmetic operations completed successfully ! "
+
+
+
+#  END # 
